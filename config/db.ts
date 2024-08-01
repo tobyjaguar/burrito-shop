d2ada69616bf966771c6da0dea1f6d3796f3d061
tobyjaguar <talgya@gmail.com>
2024-08-01 14:19:41 -0700

    comment out local db conn

--- config/db.ts
+++ config/db.ts
@@ -2,8 +2,8 @@ import mongoose, { ConnectOptions } from 'mongoose';
 import { Burrito } from '../models/burrito.js';
 import { seed } from '../startup/seed-db.js';
 
-// const url = process.env.MONGO_DB_URI as string;
-const url = 'mongodb://localhost:27017/test';
+const url = process.env.MONGO_DB_URI as string;
+// const url = 'mongodb://localhost:27017/test';
 
 export const connect = async () => {
     console.log('Connecting to MongoDB on', url);
